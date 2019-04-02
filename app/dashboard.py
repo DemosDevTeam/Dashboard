dbd02a7bad72760d4aed345f654e1238fa095990
Alex Kan <lexokan@live.unc.edu>
2019-04-01 20:50:26 -0400

    Add dashboard files to app/

---
+++ app/dashboard.py
@@ -0,0 +1,442 @@
+import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
+import scipy
+import pandas as pd
+
+from helpers import *
+
+
+# TODO: Read-in Mlab data
+# Data Maniuplation
+# Read in users and videos dataframes and select useful columns, dropping NA values
+users = pd.read_csv('data/csv/users.csv', index_col=0)
+
+users = (users[['age', 'children', 'education', 'gender',
+                'income', 'marital', 'occupation', 'race']]).dropna()
+
+videos = pd.read_csv('data/csv/videos.csv', index_col=0, encoding='latin1')
+
+# Start Flask Application
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=[
+                'https://codepen.io/chriddyp/pen/bWLwgP.css'])
+
+_app_route = '/dash-core-components/logout_button'
+
+# Create a login route
+@app.server.route('/custom-auth/login', methods=['POST'])
+def route_login():
+    data = flask.request.form
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        flask.abort(401)
+        
+    # actual implementation should verify the password.
+    # Recommended to only keep a hash in database and use something like
+    # bcrypt to encrypt the password and check the hashed results.
+
+    # Return a redirect with
+    rep = flask.redirect(_app_route)
+    
+    # store username in a cookie
+    # sign session with 
+    rep.set_cookie('custom-auth-session', username)
+    return rep 
+
+# logout route 
+@app.server.route('/custom-auth/logout', methods=['POST'])
+def route_logout():
+    rep = flask.redirect(_app_route)
+    rep.set_cookie('custom-auth-session', '', expires=0)
+    return rep
+
+# login form dash component 
+login_form = html.Div([
+    html.Form([
+        dcc.Input(placeholder='username', name='username'),
+        dcc.Input(placeholder='password', name='password', type='password'),
+        html.Button('Login', type='submit')
+    ], action='/custom-auth/login', method='post')
+])
+
+#app.layout = html.Div(id='custom-auth-frame')
+
+# @app.callback(Output('custom-auth-frame', 'children'),
+#               [Input('custom-auth-frame', 'id')])
+# def dynamic_layout(_):
+#     session_cookie = flask.request.cookies.get('custom-auth-session')
+#     if not session_cookie:
+#         # If there's no cookie we need to login.
+#         return login_form
+#     return html.Div([
+#         html.Div('Hello {}'.format(session_cookie)),
+#         dcc.LogoutButton(logout_url='/custom-auth/logout')
+#     ])
+
+
+# Specify Dash Layout
+app.layout = html.Div(
+    children=[
+        html.Div(id='custom-auth-frame'),
+        html.Div(
+            className='wrapper',
+            children=[
+                html.Div(
+                    className='sidebar',
+                    style={'data-color': 'red'},
+                    children=[
+                        html.Div(
+                            className='logo',
+                            children=[
+                                html.A(
+                                    'ACTA Solutions',
+                                    className='simple-text logo-normal',
+                                    style=dict(
+                                        textAlign='center',
+                                        color='#000000'),
+                                    ),   
+                                ]
+                        ),
+                        html.Div(
+                            className='sidebar-wrapper',
+                            children=[
+                                html.Ul(
+                                    className='nav',
+                                    children=[
+                                        html.Li(
+                                            className='active',
+                                            children=[
+                                                html.A(
+                                                    href="../examples/dashboard.html",
+                                                    children=[
+                                                        html.I(
+                                                            className="now-ui-icons design_app"),
+                                                        html.P("Dashboard")
+                                                    ]
+                                                )
+                                            ]
+                                        ),
+                                        # html.Li(
+                                        #     children=[
+                                        #         html.A(
+                                        #             href="../examples/icons.html",
+                                        #             children=[
+                                        #                 html.I(className="now-ui-icons education_atom"),
+                                        #                 html.P("Icons")
+                                        #             ]
+                                        #         )
+                                        #     ]
+                                        # ),
+                                        # html.Li(
+                                        #     children=[
+                                        #         html.A(
+                                        #             href="../examples/map.html",
+                                        #             children=[
+                                        #                 html.I(className="now-ui-icons location_map-big"),
+                                        #                 html.P("Maps")
+                                        #             ]
+                                        #         )
+                                        #     ]
+                                        # ),
+                                        # html.Li(
+                                        #     children=[
+                                        #         html.A(
+                                        #             href="../notifications.html",
+                                        #             children=[
+                                        #                 html.I(className="now-ui-icons ui-1_bell-53"),
+                                        #                 html.P("Notifications")
+                                        #             ]
+                                        #         )
+                                        #     ]
+                                        # ),
+                                        html.Li(
+                                            children=[
+                                                html.A(
+                                                    href="../examples/user.html",
+                                                    children=[
+                                                        html.I(
+                                                            className="now-ui-icons users_single-02"),
+                                                        html.P("City Profile")
+                                                    ]
+                                                )
+                                            ]
+                                        ),
+                                        html.Li(
+                                            children=[
+                                                html.A(
+                                                    href="../examples/tables.html",
+                                                    children=[
+                                                        html.I(
+                                                            className="now-ui-icons design_bullet-list-67"),
+                                                        html.P("Data Summary")
+                                                    ]
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                )
+                            ]
+                        ),
+                        # html.A("test")
+                    ],
+                ),
+                html.Div(
+                    className='main-panel',
+                    children=[
+                        html.H1('Sample City Page',
+                        style=dict(
+                            textAlign='center',
+                            color='#000000'),
+                        ),
+                        # TODO: Sentiment graph goes here
+                        
+                        # 3 Extra content plots
+                        html.Div(
+                            className='row',
+                            children=[
+                                    dcc.Graph(
+                                        id='plot1',
+                                        figure=generatePieChart(users, 'age')),
+                                    dcc.Graph(
+                                        id='plot2',
+                                        figure=generatePieChart(users, 'race')),    
+                                    dcc.Graph(
+                                        id='plot3',
+                                        figure=generatePieChart(users, 'income')
+                                    )                                                                    
+                                # html.Div(
+                                #     className='col-lg-4',
+                                #     children=[
+                                #         html.Div(
+                                #             className='card card-chart',
+                                #             children=[
+                                #                 html.Div(
+                                #                     className='card-header',
+                                #                     children=[
+                                #                         html.H5('Type1'),
+                                #                         html.H4('Plot1'),
+                                #                     ]
+                                #                 )
+                                #             ]
+                                #         )
+                                #     ]
+                                # ),
+                        # html.Div(
+                        #     className='col-lg-4',
+                        #     children=[
+                        #         html.Div(
+                        #             className='card card-chart',
+                        #             children=[
+                        #                 html.Div(
+                        #                     className='card-header',
+                        #                     children=[
+                        #                         html.H5(
+                        #                             'Type2'),
+                        #                         html.H4(
+                        #                             'Plot2'),
+                        #                         dcc.Graph(
+                        #                             id='plot2',
+                        #                             figure=generatePieChart(users, 'income')),
+                        #                         html.Div()
+                        #                         ]
+                        #                     )
+                        #                 ]
+                        #             )
+                        #         ]
+                        #     ),
+                        # html.Div(
+                        #     className='col-lg-4',
+                        #     children=[
+                        #         html.Div(
+                        #             className='card card-chart',
+                        #             children=[
+                        #                 html.Div(
+                        #                     className='card-header',
+                        #                     children=[
+                        #                         html.H5(
+                        #                             'Type3'),
+                        #                         html.H4('Plot3'),
+                        #                         dcc.Graph(
+                        #                             id='plot3',
+                        #                             figure=generatePieChart(users, 'race')),
+                        #                         ]
+                        #                     )
+                        #                 ]
+                        #             )
+                        #         ]
+                        #     ),
+                        ]
+                    ),
+
+                    # User demographics Table
+                    html.H4(
+                        'User  Demographics',
+                        style=dict(
+                            textAlign='center'
+                        )
+                    ),
+                    #generateChloropleth(),
+                    generateUserTableSummary(users),
+
+                    html.Br(),
+                    dcc.Graph(id='plot4',
+                        figure=(generateCategoricalPlot(20, 3))    
+                    ),
+
+                    dcc.Graph(id='plot5',
+                        figure=generateLinePlot()
+                    ),
+
+                    dcc.Graph(id='plot6',
+                        figure=generateBubbleChart()
+                    ),
+
+                    dcc.Graph(id='plot7',
+                        figure=generateBoxPlot()    
+                    ),
+
+                    dcc.Graph(id='plot8',
+                        figure=generatePopPyramid()    
+                    ),
+
+                    # dcc.Graph(id='plot8',
+                    #     figure=generateDistplot()
+                    # ),
+
+                    dcc.RadioItems(
+                        id='groupingDropdown',
+                        options=[
+                            {'label': 'Age', 'value': 'age'},
+                            {'label': 'Children', 'value': 'children'},
+                            {'label': 'Education', 'value': 'education'},
+                            {'label': 'Gender', 'value': 'gender'},
+                            {'label': 'Income', 'value': 'income'},
+                            {'label': 'Marital', 'value': 'marital'},
+                            {'label': 'Occupation', 'value': 'occupation'},
+                            {'label': 'Race', 'value': 'race'}
+                        ],
+                        value='age',
+                        labelStyle={
+                            'display': 'inline-block',
+                            'align-items': 'center',
+                            #'justify-content': 'center'
+                            }
+                        ),
+                    html.Br(),
+                    dcc.Graph(id='mainBar'),
+
+                    html.H4(
+                        'Video Feedback',
+                        style=dict(
+                            textAlign='center'
+                        )),
+
+
+                    # TODO: Add video table summary
+                    #generateVideoTableSummary(videos)
+                    ]
+                )
+            ]
+        ),
+    ]
+)
+
+# Add Core JS Files
+app.scripts.append_script({
+    "external_url": "../assets/js/core/jquery.min.js",
+    "external_url": "../assets/js/core/popper.min.js",
+    "external_url": "../assets/js/core/bootstrap.min.js",
+    "external_url": "../assets/js/plugins/perfect-scrollbar.jquery.min.js",
+    "external_url": "../assets/js/plugins/chartjs.min.js",
+    "external_url": "../assets/js/plugins/bootstrap-notify.js",
+    "external_url": "../assets/js/now-ui-dashboard.js?v=1.0.1",
+    "external_url" : """
+        $(document).ready(function() {
+        // Javascript method's body can be found in assets/js/demos.js
+
+        demo.initDashboardPageCharts();});
+        """
+})  
+
+@app.callback(Output('custom-auth-frame', 'children'),
+              [Input('custom-auth-frame', 'id')])
+def dynamic_layout(_):
+    session_cookie = flask.request.cookies.get('custom-auth-session')
+    if not session_cookie:
+        # If there's no cookie we need to login.
+        return login_form
+    return html.Div([
+        html.Div('Hello {}'.format(session_cookie)),
+        dcc.LogoutButton(logout_url='/custom-auth/logout')
+    ])
+    
+@app.callback(Output('mainBar', 'figure'),  
+    [Input('groupingDropdown', 'value')])
+def makeBarChart(value):
+    uniqueUsers = pd.DataFrame(users.groupby(by=value).count())
+    
+    def titleSuffix(x):
+        return {
+            'age' : 'Age',
+            'children' : 'Number of Children',
+            'education' : 'Educational Attainment',
+            'gender' : 'Gender',
+            'income' : 'Income',
+            'marital' : 'Marital Status',
+            'occupation' : 'Occupation',
+            'race' : 'Race'
+        }[x]
+
+    figure = go.Figure(
+        data=[
+            go.Bar(
+                x=uniqueUsers.index.tolist(),
+                y=uniqueUsers.iloc[:, 0].tolist(),
+                marker=dict(
+                    color='rgb(0,50,255)',
+                    line=dict(
+                        color='rgb(8,48,200)',
+                        width=1.5
+                        ),
+                    ),
+            )
+        ],
+        layout=go.Layout(
+            title="Users Grouped by " + titleSuffix(value),
+            font=dict(
+                family='Times New Roman',
+                size=22,
+                color='#7f7f7f'),
+            xaxis=dict(
+                title='Category',
+                titlefont=dict(
+                    family='Times New Roman',
+                    size=18
+                )
+            ),
+
+            yaxis=dict(
+                title='Count',
+                titlefont=dict(
+                    family='Times New Roman',
+                    size=18,
+                ),              
+            )
+        )
+    )
+
+    return figure
+
+
+# Start Dash Application
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', debug=True, port=8050)
+    # app.run_server(debug=True, port=8050, host='127.0.0.1')
+    app.run_server(debug=True)
---
+++ app/helpers.py
@@ -0,0 +1,324 @@
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+
+import dash_table
+import pandas as pd
+import numpy as np
+# import scipy
+
+# def generateDistplot():
+#     x1 = np.random.randn(200) - 2 
+#     x2 = np.random.randn(200)
+#     x3 = np.random.randn(200) + 2 
+
+#     hist_data = [x1, x2, x3]
+
+#     group_labels = ['Group 1', 'Group 2', 'Group 3']
+#     colors = ['#A56CC1', '#A6ACEC', '#63F5EF']
+
+#     # Create distplot with curve_type set to 'normal'
+#     fig = ff.create_distplot(hist_data, group_labels, colors=colors,
+#                             bin_size=.2, show_rug=False)
+
+#     # Add title
+#     fig['layout'].update(title='Hist and Curve Plot')
+
+#     return fig
+
+def generatePopPyramid():
+    women_bins = np.array([-600, -623, -653, -650, -670, -578, -541, -411, -322, -230])
+    men_bins = np.array([600, 623, 653, 650, 670, 578, 541, 360, 312, 170])
+
+    y = list(range(0, 100, 10))
+
+    layout = go.Layout(yaxis=go.layout.YAxis(title='Age'),
+                    xaxis=go.layout.XAxis(
+                        range=[-1200, 1200],
+                        tickvals=[-1000, -700, -300, 0, 300, 700, 1000],
+                        ticktext=[1000, 700, 300, 0, 300, 700, 1000],
+                        title='Number'),
+                    barmode='overlay',
+                    bargap=0.1)
+
+    data = [go.Bar(y=y,
+                x=men_bins,
+                orientation='h',
+                name='Men',
+                hoverinfo='x',
+                marker=dict(color='powderblue')
+                ),
+            go.Bar(y=y,
+                x=women_bins,
+                orientation='h',
+                name='Women',
+                text=-1 * women_bins.astype('int'),
+                hoverinfo='text',
+                marker=dict(color='seagreen')
+                )]
+
+    figure = go.Figure(
+        data=data,
+        layout=layout
+    )
+
+    return figure
+    
+
+def generateBubbleChart():
+    data = [
+        {
+        'x': [1, 3.2, 5.4, 7.6, 9.8, 12.5],
+        'y': [1, 3.2, 5.4, 7.6, 9.8, 12.5],
+        'mode': 'markers',
+        'marker': {
+            'color': [120, 125, 130, 135, 140, 145],
+            'size': [15, 30, 55, 70, 90, 110],
+            'showscale': True
+            }
+        }
+    ]
+
+    figure = go.Figure(
+        data=data
+    )
+
+    return figure
+
+# def generateChloropleth():
+#     df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/laucnty16.csv')
+#     df_sample['State FIPS Code'] = df_sample['State FIPS Code'].apply(lambda x: str(x).zfill(2))
+#     df_sample['County FIPS Code'] = df_sample['County FIPS Code'].apply(lambda x: str(x).zfill(3))
+#     df_sample['FIPS'] = df_sample['State FIPS Code'] + df_sample['County FIPS Code']
+
+#     colorscale = ["#f7fbff","#ebf3fb","#deebf7","#d2e3f3","#c6dbef","#b3d2e9","#9ecae1",
+#                 "#85bcdb","#6baed6","#57a0ce","#4292c6","#3082be","#2171b5","#1361a9",
+#                 "#08519c","#0b4083","#08306b"]
+#     endpts = list(np.linspace(1, 12, len(colorscale) - 1))
+#     fips = df_sample['FIPS'].tolist()
+#     values = df_sample['Unemployment Rate (%)'].tolist()
+
+#     fig = ff.create_choropleth(
+#         fips=fips, values=values,
+#         binning_endpoints=endpts,
+#         colorscale=colorscale,
+#         show_state_data=False,
+#         show_hover=True, centroid_marker={'opacity': 0},
+#         asp=2.9, title='USA by Unemployment %',
+#         legend_title='% unemployed'
+#     )
+
+#     return fig
+
+
+### Helper Functions for Plotting and generating Tables
+def generateBoxPlot():
+    N = 30.     # Number of boxes
+
+    c = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 360, N)]
+
+    data = [{
+        'y': 3.5*np.sin(np.pi * i/N) + i/N+(1.5+0.5*np.cos(np.pi*i/N))*np.random.rand(10), 
+        'type':'box',
+        'marker':{'color': c[i]}
+        } for i in range(int(N))]
+
+    # format the layout
+    layout = {'xaxis': {'showgrid':False,'zeroline':False, 'tickangle':60,'showticklabels':False},
+            'yaxis': {'zeroline':False,'gridcolor':'white'},
+            'paper_bgcolor': 'rgb(233,233,233)',
+            'plot_bgcolor': 'rgb(233,233,233)',
+            }
+    
+    figure = go.Figure(
+        data=data,
+        layout=layout
+    )
+
+    return figure
+
+# Generate violin data plot
+def generateCategoricalPlot(N, numCats):
+
+    randX = np.linspace(0, 1, numCats)
+    data = []
+
+    for i in range(0, 1): 
+        trace = {
+            "type": "violin",
+            "x" : randX,
+            #"y" : np.random.multinomial(N, [1/numCats]*numCats, size=1),
+            "y" : np.random.randn(N) + (.5 * i),
+            "name" : "Category " + str(i), 
+            "box" : {
+                "visible" : True
+            },
+            "meanline" : {
+                "visible" : True
+            }
+        } 
+
+        data.append(trace) 
+
+    figure = go.Figure(
+        data=data,
+        layout= dict(
+            title="", 
+            yaxis=dict(
+                zeroline=False
+            )
+        )
+    )
+
+    return figure
+
+
+
+# Generate general line plot (with random data)
+def generateLinePlot():
+    N = 200
+    randX = np.linspace(0, 1, N)
+
+    randY1 = np.random.randn(N) + 5
+    randY2 = np.random.randn(N) + 10 
+    randY3 = np.random.randn(N) + 15
+
+    title = 'Sentiment over Time'
+
+    trace1 = go.Scatter(
+        x=randX,
+        y=randY1,
+        name='Policy One'
+    )
+
+    trace2 = go.Scatter(
+        x=randX,
+        y=randY2,
+        name='Policy Two'
+    )
+
+    trace3 = go.Scatter(
+        x=randX,
+        y=randY3,
+        name='Policy Three'
+    )
+
+    data = [trace1, trace2, trace3]
+
+    figure = go.Figure(
+        data=data
+    )
+
+    return figure
+
+
+
+
+# Generate general piechart
+
+def generatePieChart(dataframe, group):
+    uniqueUsers = pd.DataFrame(dataframe.groupby(by=group).count())
+
+    labels = uniqueUsers.index.tolist()
+    values = uniqueUsers.iloc[:, 0].tolist()
+
+    figure = go.Figure(
+        data=[
+            go.Pie(
+            labels=labels,
+            values=values,
+            domain= dict(
+                x=[0, 1],
+                y=[0, 1]
+                ),
+            )
+        ],
+        layout=dict(
+            title='Citizen Percentage by ' + group.capitalize(),
+            font=dict(
+                family='Times New Roman',
+                size=15,
+                color='#7f7f7f'),        
+            autosize=False,
+            height=350,
+            width=500,
+            legend=dict(
+                #x=-0,
+                # y=0,
+                orientation='h',
+                xanchor='center',
+                #yanchor='bottom'
+                
+            ),
+            yaxis=dict(
+                title='Count',
+                titlefont=dict(
+                family='Times New Roman',
+                    size=18
+                    ),
+                #linecolor='black',
+                mirror=True,
+                ticks='outside',
+                showline=True,  
+                ),
+            xaxis=dict(
+                #linecolor='black',
+                mirror=True,
+                ticks='outside',
+                showline=True,  
+            )
+        )
+
+    )
+
+    return figure 
+
+
+# Generate HTML for the Users dataset 
+def generateUserTableSummary(dataframe):
+    table = dash_table.DataTable(
+        data=dataframe.to_dict("rows"),
+        id='userTable',
+        columns=[{"name": i, "id": i} for i in dataframe.columns],
+        style_as_list_view=True,
+        style_cell={
+            'padding': '5px'
+            },
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_cell_conditional=[{
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'
+        }]
+    )
+
+    return table
+
+# Generate HTML for the Videos dataset
+# TODO: Fix
+def generateVideoTableSummary(dataframe, max_rows=10):
+    dataframe = pd.DataFrame(dataframe.items())
+    table = dash_table.DataTable(
+        data=dataframe.to_dict("rows"),
+        id='videoTable',
+        columns=[{"name": i, "id": i} for i in dataframe.columns],
+        style_as_list_view=True,
+        style_cell={
+            'padding': '5px'
+            },
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_cell_conditional=[{
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'
+        }]
+    )
+
+    return table   
